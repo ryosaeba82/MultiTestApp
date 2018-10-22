f785029cebe12c337b942649b19cebec28a0a67d
Marco PICCIAU <Marco.PICCIAU@ingenico.com>
2018-10-22 15:19:50 +0200

    * Inserito controllo per lancio SplashScreen

--- app/src/main/java/com/starware/mpicciau/multitestapp/MainActivity.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.starware.mpicciau.multitestapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new BasicMenuFragment(), "Basic");
+        adapter.addFragment(new IntentMenuFragment(), "Intent");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
