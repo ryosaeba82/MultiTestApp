2d003cbd819f9a7052916168ecd85ea8336cf11a
ryosaeba82 <marcopicci@gmail.com>
2018-10-23 17:41:42 +0200

    Implementato TabLayout

--- app/src/main/java/com/starware/mpicciau/multitestapp/MainActivity.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.starware.mpicciau.multitestapp;
 
+
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +9,16 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private TabAdapter adapter;
-    private TabLayout tabLayout;
     private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
 
         adapter = new TabAdapter(getSupportFragmentManager());
         adapter.addFragment(new BasicMenuFragment(), "Basic");
