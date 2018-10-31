40ced3f18ee5d9f949ce2bdec0c90e07957bb853
Marco PICCIAU <Marco.PICCIAU@ingenico.com>
2018-10-31 09:30:49 +0100

    * Corretta gestione bottoni nei fragment

--- app/src/main/java/com/starware/mpicciau/multitestapp/BasicMenuFragment.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/BasicMenuFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,11 +24,16 @@ public class BasicMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basic_menu, container, false);
+        View view =  inflater.inflate(R.layout.fragment_basic_menu, container, false);
+        Button button = (Button) view.findViewById(R.id.button_exit);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                System.exit(0);
+            }
+        });
+        return view;
     }
-
-    public void onExitClick(View v){
-        System.exit(0);
-    }
-
 }
--- app/src/main/java/com/starware/mpicciau/multitestapp/IntentMenuFragment.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/IntentMenuFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -24,11 +25,16 @@ public class IntentMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intent_menu, container, false);
-    }
+        View view =  inflater.inflate(R.layout.fragment_intent_menu, container, false);
+        Button button = view.findViewById(R.id.button_call);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),CallActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
 
-    public void onCallClick(View v){
-        Intent intent = new Intent(getActivity(),CallActivity.class);
-        getActivity().startActivity(intent);
+        return view;
     }
 }
