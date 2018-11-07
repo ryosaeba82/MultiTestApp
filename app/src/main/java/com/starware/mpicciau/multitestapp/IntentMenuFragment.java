38a5e510fbab04d32b6dbf691503ae93b6608296
Marco PICCIAU <Marco.PICCIAU@ingenico.com>
2018-11-07 14:49:27 +0100

    * Iniziata implementazione GPS

--- app/src/main/java/com/starware/mpicciau/multitestapp/IntentMenuFragment.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/IntentMenuFragment.java
@@ -20,18 +20,32 @@ public class IntentMenuFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private void Geolocation_execute(View view){
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_intent_menu, container, false);
-        Button button = view.findViewById(R.id.button_call);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        //Button Call management
+        Button button_call = view.findViewById(R.id.button_call);
+        button_call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(),CallActivity.class);
                 getActivity().startActivity(intent);
+            }
+        });
+
+        //Button Geo management
+        Button button_geo = view.findViewById(R.id.button_geo);
+        button_geo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Geolocation_execute(view);
             }
         });
 
