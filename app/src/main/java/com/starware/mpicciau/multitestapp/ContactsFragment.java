f8f5e8e0bb3e70a80ea7f573ee72cdb1cc6f1ca1
ryosaeba82 <marcopicci@gmail.com>
2018-11-18 22:41:17 +0100

    * Proseguiti lavori

--- app/src/main/java/com/starware/mpicciau/multitestapp/ContactsFragment.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/ContactsFragment.java
@@ -1,28 +1,109 @@
 package com.starware.mpicciau.multitestapp;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class ContactsFragment extends Fragment {
+
+    ListView contactsListView;
+    EditText contactText;
 
     public ContactsFragment() {
         // Required empty public constructor
     }
 
+    private void ShowContact(AdapterView<?> arg0, View arg1, int position, long arg3){
+
+    }
+
+    //Ricerca dei contatti utilizzando come stringa di ricerca il contenuto della edittext
     private void SearchContact_execute(View view){
+
+        if(contactText.getText().toString().length() == 0)
+        {
+            Toast.makeText(getActivity(),getString(R.string.alert_contactsearch_empty),Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        int permissionGranted = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS);
+        if(permissionGranted == PackageManager.PERMISSION_DENIED){
+            Toast.makeText(getActivity(),getString(R.string.alert_no_permission),Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //ricerca dei contatti con query SQL
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        Uri contentUri = ContactsContract.Contacts.CONTENT_URI;
+        String contactId = ContactsContract.Contacts._ID;
+        String contactLookupKey = ContactsContract.Contacts.LOOKUP_KEY;
+        String contactName = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? ContactsContract.Contacts.DISPLAY_NAME_PRIMARY : ContactsContract.Contacts.DISPLAY_NAME;
+        String [] projection = {contactId,contactLookupKey,contactName};
+        String sqlWhere = contactName + " LIKE ?";
+        String [] sqlWhereConditions = {"%" + contactText.getText().toString() + "%"};
+        Cursor cursor = contentResolver.query(contentUri,projection,sqlWhere,sqlWhereConditions,null);
+
+        if ( cursor.getCount() > 0 )
+        {
+            ArrayList<String> data = new ArrayList<String>();
+            HashMap<String, Long> nameToId = new HashMap<String, Long>();
+            HashMap<String, String> nameToLookupKey = new HashMap<String, String>();
+            while ( cursor.moveToNext() )
+            {
+                String name = cursor.getString(cursor.getColumnIndex(contactName));
+                Long id = cursor.getLong(cursor.getColumnIndex(contactId));
+                String lookupKey = cursor.getString(cursor.getColumnIndex(contactLookupKey));
+                data.add(name);
+                nameToId.put(name,id);
+                nameToLookupKey.put(name,lookupKey);
+            }
+
+            //visualizzazione contatti
+            int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
+            contactsListView.setAdapter(new ArrayAdapter<String>(getActivity(),layout,data));
+            //impostazione evento click sul contatto
+            contactsListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> arg0, View arg1,
+                                        int position, long arg3){
+                    ShowContact(arg0, arg1, position, arg3);
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getActivity(), getString(R.string.alert_no_contacts), Toast.LENGTH_LONG).show();
+        }
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        //ListView con elenco dei contatti
+        contactsListView = view.findViewById(R.id.contactListView);
+
+        //EditText per ricerca dei contatti
+        contactText = view.findViewById(R.id.contactSearchEdit);
 
         //Button Search contact
         Button button_search = view.findViewById(R.id.button_searchContact);
--- app/src/main/java/com/starware/mpicciau/multitestapp/MainActivity.java
+++ app/src/main/java/com/starware/mpicciau/multitestapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.starware.mpicciau.multitestapp;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
     private TabAdapter adapter;
     private ViewPager viewPager;
     private CustomTabLayout tabLayout;
@@ -26,5 +33,36 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        getPermissionToReadUserContacts();
+    }
+
+    public void getPermissionToReadUserContacts() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
